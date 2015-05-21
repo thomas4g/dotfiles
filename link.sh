e6ff4304764bb6167d7f21634509bc1904e26c15
Thomas Shields <thomas@etcm-tas-d01.ctisl.gtri.org>
2015-05-21 10:11:35 -0400

    updated linker script and added basic profile

--- link.sh
+++ link.sh
@@ -1,5 +1,7 @@
 ln -is dotfiles/.vim .vim
 ln -is dotfiles/.bashrc .bashrc
+ln -is dotfiles/.bash_aliases .bash_aliases
+ln -is dotfiles/.bash_profile .bash_profile
 ln -is dotfiles/.vimrc .vimrc
 ln -is dotfiles/.viminfo .viminfo
 ln -is dotfiles/.Xmodmap .Xmodmap
